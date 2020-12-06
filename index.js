eaa7df7ecd94644033b4eb51363ef4d5920a5915
psupri <46396932+psupri@users.noreply.github.com>
2020-12-06 19:42:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const server = http.createServer((req,res) => {
 res.end("<h1>Hello World from OpenShift NodeJS App</h1>");
 });
 
-server.listen(8000,() => { console.log("Listening to requests on Port 8000");
+server.listen(8080,() => { console.log("Listening to requests on Port 8080");
 });
